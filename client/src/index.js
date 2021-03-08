9f32e98b8ce59b4579410f3297e8e7aa113295c7
Matt Phillips <spectremp24@gmail.com>
2021-03-08 09:57:31 -0500

    added authentication on client and server side

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reduxThunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import reducer from '../src/reducers/index';
+
+let store = createStore(reducer, {},  
+  compose(
+    applyMiddleware(reduxThunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
 
 serviceWorker.unregister();
