24d0acd244863fccb877050659297becb19b0b3e
Matt Phillips <spectremp24@gmail.com>
2021-03-25 20:42:39 -0400

    change app.js for heroku

--- server/app.js
+++ server/app.js
@@ -10,9 +10,15 @@ app.use(function(req, res, next) {
 app.use(require('./routes/authentications'));
 app.use(require('./routes/assets'));
 
-app.listen(3001, ()=>{
-    console.log(`listening on port 3001`);
-})
+// app.listen(3001, ()=>{
+//     console.log(`listening on port 3001`);
+// })
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 
 // const jwt = require('jwt-simple'); // create a jwt token
